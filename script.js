90b264cc9ce044fb85906c50ddc2f794dcf03682
VICTOR EDUARDO PEIXER MUNARIM <victor_munarim@estudante.sc.senai.br>
2025-03-05 14:44:13 -0300

    primeiros arquivos

---
+++ script.js
@@ -0,0 +1,23 @@
+const canvas = document.getElementById('jogoCanvas')
+const ctx = canvas.getContext('2d')
+
+let gameOver = false
+
+
+
+
+
+
+
+
+
+
+
+
+function loop () {
+    if (!gameOver){
+
+    }
+}
+
+loop()
