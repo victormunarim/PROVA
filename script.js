c56aa3c652378ab2105a43ceecd0aea81310c5fa
VICTOR EDUARDO PEIXER MUNARIM <victor_munarim@estudante.sc.senai.br>
2025-03-05 17:38:09 -0300

    movimentacao da bola e colisoes

--- script.js
+++ script.js
@@ -3,6 +3,26 @@ const ctx = canvas.getContext('2d')
 
 let gameOver = false
 
+
+function reiniciarJogo() {
+    gameOver = false
+    bola.x = canvas.width / 2;
+    bola.y = canvas.height - 40;
+    bola.velocidadeX = 3;
+    bola.velocidadeY = -3;
+    barraPrincipal.x = 175;
+    barraPrincipal.y = 480;
+
+    barraInimiga1.existe = true;
+    barraInimiga2.existe = true;
+    barraInimiga3.existe = true;
+
+    loop();
+}
+
+
+
+
 document.addEventListener("click", (e) => {
     if (gameOver == true) {
         location.reload()
@@ -86,21 +106,27 @@ class BarrasInimigas extends Jogo {
         this.existe = true
     }
     desenharBarrasInimigas1 = function (posicao) {
-        for (posicao = 0; posicao < 400; posicao += 70) {
-            ctx.fillStyle = 'red'
-            ctx.fillRect(posicao, this.y, this.largura, this.altura)
+        if (this.existe) {
+            for (posicao = 0; posicao < 400; posicao += 70) {
+                ctx.fillStyle = 'red'
+                ctx.fillRect(posicao, this.y, this.largura, this.altura)
+            }
         }
     }
     desenharBarrasInimigas2 = function (posicao) {
-        for (posicao = 0; posicao < 400; posicao += 70) {
-            ctx.fillStyle = 'orange'
-            ctx.fillRect(posicao, this.y, this.largura, this.altura)
+        if (this.existe) {
+            for (posicao = 0; posicao < 400; posicao += 70) {
+                ctx.fillStyle = 'orange'
+                ctx.fillRect(posicao, this.y, this.largura, this.altura)
+            }
         }
     }
     desenharBarrasInimigas3 = function (posicao) {
-        for (posicao = 0; posicao < 400; posicao += 70) {
-            ctx.fillStyle = 'blue'
-            ctx.fillRect(posicao, this.y, this.largura, this.altura)
+        if (this.existe) {
+            for (posicao = 0; posicao < 400; posicao += 70) {
+                ctx.fillStyle = 'blue'
+                ctx.fillRect(posicao, this.y, this.largura, this.altura)
+            }
         }
     }
 }
@@ -110,13 +136,43 @@ class Bola extends Jogo {
         super(x, y)
         this.r = r
         this.velocidadeX = 3;
-        this.velocidadeY = 3
+        this.velocidadeY = -3
     }
     desenharBola() {
         ctx.fillStyle = 'white';
         ctx.beginPath();
         ctx.arc(this.x, this.y, this.r, 0, Math.PI * 2);
         ctx.fill();
+    }
+    mover() {
+        this.x += this.velocidadeX
+        this.y += this.velocidadeY
+
+        if (this.x + this.r > canvas.width || this.x - this.r < 0) {
+            this.velocidadeX = -this.velocidadeX;
+        }
+        if (this.y - this.r < 0) {
+            this.velocidadeY = -this.velocidadeY;
+        }
+
+        if (this.y + this.r > barraPrincipal.y &&
+            this.x > barraPrincipal.x &&
+            this.x < barraPrincipal.x + barraPrincipal.largura) {
+            this.velocidadeY = -this.velocidadeY;
+        }
+        barrasInimigas.forEach(barra => {
+            if (barra.existe &&
+                this.y - this.r < barra.y + barra.altura &&
+                this.y + this.r > barra.y) {
+                this.velocidadeY = -this.velocidadeY;
+                barra.existe = false;
+            }
+        });
+    }
+    bolaFora() {
+        if (this.y > canvas.height) {
+            reiniciarJogo()
+        }
     }
 }
 
@@ -125,6 +181,8 @@ const barraPrincipal = new BarraPrincipal(175, 480, 50, 10)
 const barraInimiga1 = new BarrasInimigas(10, 10, 50, 10)
 const barraInimiga2 = new BarrasInimigas(10, 30, 50, 10)
 const barraInimiga3 = new BarrasInimigas(10, 50, 50, 10)
+
+const barrasInimigas = [barraInimiga1, barraInimiga2, barraInimiga3]
 
 const bola = new Bola(canvas.width / 2, canvas.height - 40, 10);
 
@@ -137,6 +195,8 @@ function loop() {
         barraInimiga2.desenharBarrasInimigas2()
         barraInimiga3.desenharBarrasInimigas3()
         bola.desenharBola()
+        bola.mover()
+        bola.bolaFora()
         requestAnimationFrame(loop)
 
     }
